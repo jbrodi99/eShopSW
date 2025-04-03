03a874d79f575ab3c9642361abb9ab9969c72e34
jbrodi99 <juancruz.99.jcr@gmail.com>
2025-04-03 15:46:58 +0000

    feat:all-products-add-in-model-from-main-controller

--- e.shop.geonosis/webapp/controller/Main.controller.js
+++ e.shop.geonosis/webapp/controller/Main.controller.js
@@ -6,17 +6,35 @@ sap.ui.define([
 
     return Controller.extend("com.geonosis.shop.e.shop.geonosis.controller.Main", {
         onInit() {
-            const oDataImgs = {
-                images: [
-                    { src: "../assets/images/banner-1-sw.png" },
-                    { src: "../assets/images/banner-2-sw.png" },
-                    { src: "../assets/images/banner-3-sw.png" }
-                ]
-            };
+          const oDataImgs = {
+            images: [
+              { src: "../assets/images/banner-1-sw.png" },
+              { src: "../assets/images/banner-2-sw.png" },
+              { src: "../assets/images/banner-3-sw.png" }
+            ]
+          };
 
-            const oModelImgs = new JSONModel(oDataImgs);
-            this.getView().setModel(oModelImgs, "view");
+          const oModelImgs = new JSONModel(oDataImgs);
+          this.getView().setModel(oModelImgs, "view");
 
+          const oCatalogModel = this.getOwnerComponent().getModel("catalog");
+          const oCatalogData = oCatalogModel ? oCatalogModel.getData() : { catalog: { categories: [] } };
+          
+          const aProducts = this._extraxtProducts(oCatalogData.catalog.categories);
+
+          const oProductModel = new JSONModel ({products: aProducts});
+
+          this.getView().setModel(oProductModel, "productsModel");
+
+        },
+
+        _extraxtProducts: (aCategories) => {
+          return aCategories.reduce((aProducts, category) => {
+            category.subcategories.forEach(subCategory => {
+              aProducts.push(...subCategory.products);
+            });
+            return aProducts;
+          }, []);
         }
     });
 });
