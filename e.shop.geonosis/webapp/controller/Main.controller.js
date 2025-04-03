bd4e6ba38226dce956b3e1fb5bc801ab93f929f8
jbrodi99 <juancruz.99.jcr@gmail.com>
2025-04-03 02:26:51 +0000

    feat:carousel-img-main-view

--- e.shop.geonosis/webapp/controller/Main.controller.js
+++ e.shop.geonosis/webapp/controller/Main.controller.js
@@ -1,10 +1,22 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+], (Controller, JSONModel) => {
     "use strict";
 
     return Controller.extend("com.geonosis.shop.e.shop.geonosis.controller.Main", {
         onInit() {
+            const oDataImgs = {
+                images: [
+                    { src: "../assets/images/banner-1-sw.png" },
+                    { src: "../assets/images/banner-2-sw.png" },
+                    { src: "../assets/images/banner-3-sw.png" }
+                ]
+            };
+
+            const oModelImgs = new JSONModel(oDataImgs);
+            this.getView().setModel(oModelImgs, "view");
+
         }
     });
 });
