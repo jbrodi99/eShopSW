ed884a8ed441682150583657fb9b266ad8b62841
jbrodi99 <juancruz.99.jcr@gmail.com>
2025-04-06 17:03:38 +0000

    test:Controller.onInit: productsModel leaded

--- e.shop.geonosis/webapp/controller/Products.controller.js
+++ e.shop.geonosis/webapp/controller/Products.controller.js
@@ -6,16 +6,20 @@ sap.ui.define([
   
     return Controller.extend("com.geonosis.shop.e.shop.geonosis.controller.Products", {
       onInit: function() {
-        this.getOwnerComponent().getRouter().getRoute("products").attachPatternMatched(this._onRouteMatched, this);
-        console.log("cargue la ruta");
-      },
-      
-      _onRouteMatched: function (oEvent) {
-        const subcategoryId = oEvent.getParameter("arguments").subcategoryId;
-      
-        const oProductsModel = products.createFilteredProductsModel(this.getOwnerComponent(), subcategoryId);
-      
+        // this.getOwnerComponent().getRouter().getRoute("products").attachPatternMatched(this._onRouteMatched, this);
+        // console.log("cargue la ruta");
+        let oProductsModel = products.createProductsModel(this.getOwnerComponent());
         this.getView().setModel(oProductsModel, "products");
       }
+
+
+      
+      // _onRouteMatched: function (oEvent) {
+      //   const subcategoryId = oEvent.getParameter("arguments").subcategoryId;
+      
+      //   const oProductsModel = products.createFilteredProductsModel(this.getOwnerComponent(), subcategoryId);
+      
+      //   this.getView().setModel(oProductsModel, "products");
+      // }
     });
   });
