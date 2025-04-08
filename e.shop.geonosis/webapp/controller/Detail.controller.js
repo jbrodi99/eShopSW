012cab4124c99f201e1cd39260aae73819170c43
jbrodi99 <juancruz.99.jcr@gmail.com>
2025-04-08 20:12:11 +0000

    feat:add-Detail-view

---
+++ e.shop.geonosis/webapp/controller/Detail.controller.js
@@ -0,0 +1,47 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+  ], (Controller, JSONModel) => {
+    "use strict";
+  
+    return Controller.extend("com.geonosis.shop.e.shop.geonosis.controller.Detail", {
+      onInit: function() {
+
+        let mockProduct = {
+            "id": "prod12",
+            "name": "Modelo Destructor Estelar",
+            "price": 499.99,
+            "currency": "USD",
+            "description": "Magnífico modelo a escala del Destructor Estelar con múltiples detalles, ideal para los coleccionistas.",
+            "image": [
+            "assets/images/luke.png",
+            "assets/images/luke.png",
+            "assets/images/luke.png"
+            ],
+            "mainImage": "assets/images/luke.png",
+            "stock": 3,
+            "cantidadVendida": 5,
+            "rating": 4.9,
+            "details": {
+            "scale": "1:144",
+            "material": "Plástico y metal",
+            "manufacturer": "Starship Models",
+            "weight": "3.5 kg"
+            },
+            "tags": ["modelo", "Destructor Estelar", "Star Wars", "coleccionable"],
+            "variants": []
+        };
+
+        let detailsObject = mockProduct.details;
+        let detailsArray = Object.entries(detailsObject).map(([key, value]) => {
+          return { key, value };
+        });
+        mockProduct.detailsArray = detailsArray;
+
+        let oProductModel = new JSONModel({product: mockProduct});
+
+        console.log(oProductModel);
+        this.getView().setModel(oProductModel, "product");
+      }
+    });
+  });
